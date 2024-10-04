a232f63e87aeb751435816c0d415d2b873910342
Jean-Louis Lamezec <jeanlouis.lmzc@gmail.com>
2024-10-04 14:01:54 +0200

    feat(lyon): add hourly visits daily dump

--- dags/lyon.py
+++ dags/lyon.py
@@ -1,18 +1,17 @@
 import pendulum
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 from data_utils.matomo_pull.matomo_helper import fetch_and_dump_data
-from data_utils.alerting.alerting import task_failed
 
-
-matomo_site_id = 3
-matomo_db_name = "airflow_db_test"
+matomo_site_id = Variable.get("lyon_matomo_site_id")
+matomo_db_name = Variable.get("lyon_matomo_db_name")
 
 with DAG(
-        dag_id='matomo_test',
+        dag_id='lyon',
         default_args={'owner': 'airflow'},
         schedule='@daily',
-        start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),  # Correction du type de start_date
+        start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
         catchup=True
 ) as dag:
 
@@ -21,7 +20,6 @@ with DAG(
         python_callable=fetch_and_dump_data,
         op_args=[f"{matomo_site_id}", f"{matomo_db_name}", "{{ ds }}"],
         dag=dag,
-        on_failure_callback=task_failed,
     )
 
     fetch_matomo_data
