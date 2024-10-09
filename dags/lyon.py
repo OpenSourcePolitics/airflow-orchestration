b3d9f1b23ee98b7e45827b735c30ff0aea1beed0
Jean-Louis Lamezec <jeanlouis.lmzc@gmail.com>
2024-10-09 15:45:27 +0200

    feat(lyon): add task failed callback

--- dags/lyon.py
+++ dags/lyon.py
@@ -3,6 +3,7 @@ from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 from data_utils.matomo_pull.matomo_helper import fetch_and_dump_data
+from data_utils.alerting.alerting import task_failed
 
 matomo_site_id = Variable.get("lyon_matomo_site_id")
 matomo_db_name = Variable.get("lyon_matomo_db_name")
@@ -20,6 +21,7 @@ with DAG(
         python_callable=fetch_and_dump_data,
         op_args=[f"{matomo_site_id}", f"{matomo_db_name}", "{{ ds }}"],
         dag=dag,
+        on_failure_callback=task_failed,
     )
 
     fetch_matomo_data
