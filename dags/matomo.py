df7010c2e40f19b445bfc40e6514a947406bd7c4
Jean-Louis Lamezec <jeanlouis.lmzc@gmail.com>
2024-10-10 16:30:22 +0200

    rename dag matomo test post push tag

--- dags/matomo.py
+++ dags/matomo.py
@@ -9,7 +9,7 @@ matomo_site_id = 3
 matomo_db_name = "airflow_db_test"
 
 with DAG(
-        dag_id='matomo_test',
+        dag_id='matomo_test_preprod',
         default_args={'owner': 'airflow'},
         schedule='@daily',
         start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),  # Correction du type de start_date
