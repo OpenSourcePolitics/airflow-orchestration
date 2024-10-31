88ba01d3679c03a2bdd29d1ff5c76286d409d54e
Jean-Louis Lamezec <jeanlouis.lmzc@gmail.com>
2024-10-31 17:12:10 +0100

    feat(airbyte): integration

--- dags/connections/airbyte/airbyte_connection_id_retriever.py
+++ dags/connections/airbyte/airbyte_connection_id_retriever.py
@@ -1,57 +1,28 @@
 import requests
-from airflow.models import Variable
 from airflow.hooks.base import BaseHook
+from airflow.models import Variable
 
-# Function to dynamically retrieve the token via the Airbyte API. Token are short-lived, 3 minutes.
-def get_airbyte_token():
-    # Retrieve API connection details from Airflow connections
-    connection = BaseHook.get_connection('airbyte_api')
-    api_url = connection.host
-    client_id = connection.login
-    client_secret = connection.password
-
-    # Request to get the token via Airbyte API
-    token_url = f"{api_url}/v1/token"  # URL to get the token according to your API spec
-    token_data = {
-        'client_id': client_id,
-        'client_secret': client_secret
-    }
-
-    response = requests.post(token_url, json=token_data)
-
-    if response.status_code == 200:
-        token = response.json().get('access_token')
-        return token
-    else:
-        raise Exception("Failed to retrieve token")
 
 # Function to get the Airbyte connection ID
 def get_airbyte_connection_id(connection_name):
     # Dynamically retrieve the token
-    token = get_airbyte_token()
-
-    # Retrieve API connection details from Airflow
     connection = BaseHook.get_connection('airbyte_api')
+    # To understand why schema : https://airflow.apache.org/docs/apache-airflow-providers-airbyte/stable/_modules/airflow/providers/airbyte/hooks/airbyte.html
+    token = Variable.get("airbyte_token")
     api_url = connection.host
 
-    # Get the Workspace ID from an Airflow variable
-    workspace_id = Variable.get("workspace_id")
-
     headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json',
-    }
-
-    data = {
-        'workspaceId': f'{workspace_id}'
+        "accept": "application/json",
+        "authorization": f'Bearer {token}'
     }
 
     # API call to get the list of connections
-    response = requests.post(f"{api_url}/v1/connections/list", headers=headers, json=data)
+    # Be sure to use the public endpoint here : https://docs.airbyte.com/using-airbyte/configuring-api-access
+    response = requests.get(f"{api_url}connections", headers=headers)
 
     if response.status_code == 200:
-        connections = response.json().get('connections', [])
+        connections = response.json().get('data', [])
         for conn in connections:
             if conn['name'] == connection_name:
                 return conn['connectionId']
-    raise Exception("Connection not found")
+    raise Exception(f"Connection not found with response: {response.json()} ")
--- dags/grand_nancy.py
+++ dags/grand_nancy.py
@@ -1,5 +1,6 @@
 import pendulum
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
@@ -9,39 +10,39 @@ from data_utils.github_helper import get_github_token, trigger_workflow
 # Get the GitHub token using the function
 github_token = get_github_token()
 
+env = Variable.get("environment")
 
-AIRBYTE_CONNECTION_ID = get_airbyte_connection_id("[Decidim] - Grand Nancy")
-AIRBYTE_AIRFLOW_CONN_ID = 'airbyte_local'
+AIRBYTE_CONNECTION_ID = get_airbyte_connection_id(f"[Decidim]-[{env.upper()}] - Grand Nancy")
+AIRBYTE_AIRFLOW_CONN_ID = 'airbyte_api'
 
 # DAG Configuration
 with DAG(
-        dag_id='grand_nancy_dag_v2',
+        dag_id='grand_nancy',
         default_args={'owner': 'airflow'},
         schedule='@daily',
         start_date=pendulum.today('UTC').add(days=-1)
 ) as dag:
 
-    # trigger_airbyte_sync = AirbyteTriggerSyncOperator(
-    #     task_id='airbyte_trigger_sync',
-    #     airbyte_conn_id=AIRBYTE_AIRFLOW_CONN_ID,
-    #     connection_id=AIRBYTE_CONNECTION_ID,
-    #     asynchronous=True
-    # )
-    #
-    # wait_for_sync_completion = AirbyteJobSensor(
-    #     task_id='airbyte_check_sync',
-    #     airbyte_conn_id='airflow-call-to-airbyte-example',
-    #     airbyte_job_id=trigger_airbyte_sync.output
-    # )
+    trigger_airbyte_sync = AirbyteTriggerSyncOperator(
+        task_id='airbyte_trigger_sync',
+        airbyte_conn_id=AIRBYTE_AIRFLOW_CONN_ID,
+        connection_id=AIRBYTE_CONNECTION_ID,
+        asynchronous=True
+    )
 
-    # Task to trigger the GitHub Action
+    wait_for_sync_completion = AirbyteJobSensor(
+        task_id='airbyte_check_sync',
+        airbyte_conn_id=AIRBYTE_AIRFLOW_CONN_ID,
+        airbyte_job_id=trigger_airbyte_sync.output
+    )
+
+    #Task to trigger the GitHub Action
     run_dbt_github_action = PythonOperator(
         task_id="trigger_github_action",
         python_callable=trigger_workflow,
-        op_args=['grand_nancy_models_run.yml'],  # Pass the YAML file name as an argument
+        op_args=[f'grand_nancy_models_run_{env}.yml'],  # Pass the YAML file name as an argument for the proper env
         params={'token': f"{github_token}"},  # Pass the GitHub token as a parameter
         dag=dag
     )
 
-    # trigger_airbyte_sync >> wait_for_sync_completion >>
-    run_dbt_github_action
+    trigger_airbyte_sync >> wait_for_sync_completion >> run_dbt_github_action
