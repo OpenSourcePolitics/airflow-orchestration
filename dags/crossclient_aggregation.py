687833dbd8686298f2486fef1df1c0feb2d95390
ailepet <hellpe@users.noreply.github.com>
2025-06-11 14:36:22 +0200

    still WIP

---
+++ dags/crossclient_aggregation.py
@@ -0,0 +1,25 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from data_utils.alerting.alerting import task_failed
+from data_utils.crossclient_aggregation.crossclient_pull import create_aggregated_tables
+
+connection_name="main_db_cluster_name"
+
+with DAG(
+        dag_id='crossclient_aggregation',
+        default_args={'owner': 'airflow'},
+        schedule='45 0 * * *',
+        start_date=pendulum.datetime(2024, 11, 15, tz="UTC"),
+        catchup=True
+) as dag:
+
+    aggregate_crossclient_data = PythonOperator(
+        task_id='create_aggregated_tables',
+        python_callable=create_aggregated_tables,
+        op_args=[f"{connection_name}"],
+        dag=dag,
+        on_failure_callback=task_failed,
+    )
+
+    aggregate_crossclient_data
--- dags/data_utils/crossclient_aggregation/crossclient_pull.py
+++ dags/data_utils/crossclient_aggregation/crossclient_pull.py
@@ -1,3 +1,48 @@
 import pandas as pd
 from ..postgres_helper.client_db_list import database_name
 from ..postgres_helper.postgres_helper import dump_data_to_postgres, get_postgres_connection
+
+queries = {
+    "all_users": "SELECT id AS decidim_user_id, email, created_at, confirmed, sign_in_count, deleted_at, blocked, spam, date_of_birth, gender FROM prod.users",
+    "budgets": "SELECT id AS decidim_budget_id, title, amount, is_selected, categories, ps_title, FROM prod.budgets",
+}
+
+def retrieve_all_clients_data(query, engine):
+
+    connection = engine.connect()
+    df = pd.read_sql(query, connection)
+    connection.close()
+
+    return df
+
+def aggregate_data_by_client(client, df):
+    df = df.assign(client='lyon')
+    df.insert(0, 'client', df.pop('client'))
+
+
+def fetch_and_dump_crossclient_data(frames, query, client):
+    fetch_db_name = database_name[client]
+    dump_db_name = 'aggregated_client_data'
+    engine = get_postgres_connection("main_db_cluster_name", fetch_db_name)
+    connection = engine.connect()
+
+    df = retrieve_all_clients_data(query, engine)
+    df = aggregate_data_by_client(client, df)
+    frames.append(df)
+
+    result = pd.concat(frames)
+
+    connection.close()
+
+
+    table_name = f'aggregate_by_{query}'
+    engine = get_postgres_connection("main_db_cluster_name", dump_db_name)
+    connection = engine.connect()
+    dump_data_to_postgres(connection, result, table_name, schema='public', if_exists='replace')
+    connection.close()
+
+def create_aggregated_tables(queries, clients):
+    for query in queries:
+        frames = []
+        for client in clients:
+            fetch_and_dump_crossclient_data(frames, query, client)
