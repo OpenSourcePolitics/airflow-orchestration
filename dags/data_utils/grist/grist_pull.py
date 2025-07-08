0848ea88b27e4b23ceb6f7977590989bf62aff25
ailepet <hellpe@users.noreply.github.com>
2025-07-08 15:30:40 +0200

    pull a list of all clients from Grist

--- dags/data_utils/grist/grist_pull.py
+++ dags/data_utils/grist/grist_pull.py
@@ -8,17 +8,17 @@ from airflow.models import Variable
 connection = BaseHook.get_connection("grist_osp")
 grist_api_key = connection.password
 grist_server = connection.host
-grist_ca_doc_id = Variable.get("grist_ca_doc_id")
+grist_ca_doc_id = Variable.get("grist_all_clients_doc_id")
 
 # Get api key from your Profile Settings, and run with GRIST_API_KEY=<key>
 api = GristDocAPI(grist_ca_doc_id, server=grist_server, api_key=grist_api_key)
 
 
 def fetch_and_dump_data(connection_name):
-    data = api.fetch_table('Suivi_CA_par_clients')
+    data = api.fetch_table('Liste_de_tous_les_clients')
     df = pd.DataFrame(data)
 
-    df['Prestations_2024'] = df['Prestations_2024'].astype(str)
+    df['Prestations_2025'] = df['Prestations_2025'].astype(str)
 
     # Add boolean columns for each field
     fields = ['Abo Decidim', 'Abo Grist', 'Abo Metabase', 'Bénévolat',
@@ -27,11 +27,11 @@ def fetch_and_dump_data(connection_name):
 
     # Ajouter des colonnes booléennes pour chaque champ
     for field in fields:
-        df[field] = df['Prestations_2024'].apply(lambda x: field in x)
+        df[field] = df['Prestations_2025'].apply(lambda x: field in x)
 
     engine = get_postgres_connection(connection_name, "aggregated_client_data")
     connection = engine.connect()
-    table_name = "grist_test_ca"
+    table_name = "all_clients"
 
     drop_table_in_postgres(connection, table_name)
     dump_data_to_postgres(connection, df, table_name)
